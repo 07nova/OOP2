a0b9b7307e1acf3eddbc08e32380b694abc17706
07nova <koko.baheneka@strathmore.edu>
2016-04-27 21:28:50 +0300

    Added files via upload

---
+++ scope0.cpp
@@ -0,0 +1,14 @@
+#include <iostream >
+
+ using namespace std;
+
+ int main()
+ {
+ int arg1;
+ arg1 = -1;
+ int x, y, z;
+ char myDouble = 5;
+ char arg2 = 2;
+ cout << arg1 << "\n";
+ return 0;
+ }
